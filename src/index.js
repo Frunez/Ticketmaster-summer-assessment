319fc446841550ea731e879d6e326e53aa2196b7
frunez <francesco.nunez@gmail.com>
2017-05-15 00:03:30 +0100

    completed basic webpage

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ function FestivalList(){
   this.festivalHTML = "";
   this.imagesJSON = images;
   this.imagesHTML = "";
+  this.videosJSON = videos;
+  this.videosHTML = "";
 }
 
 FestivalList.prototype.list = function(){
@@ -12,21 +14,44 @@ FestivalList.prototype.list = function(){
     this.festivalHTML +=
     "<a href=" + this.festivalJSON[i].url + ">" +
     "<li class='festival list-item' id='festival-" + i + "'>" +
-    "<span class='festival-name'>" + this.festivalJSON[i].name + "</span>" +
-    "<span class='festival-location'> &nbsp &nbsp" + this.festivalJSON[i].location + "</span>" +
-    "<span class='festival-date'>" + this.festivalJSON[i].date + "</span>" +
+    "<div class='festival-name'>" + this.festivalJSON[i].name + "</div>" +
+    "<div class='festival-location'> &nbsp &nbsp" + this.festivalJSON[i].location + "</div>" +
+    "<div class='festival-date'>" + this.festivalJSON[i].date + "</div>" +
     "</li></a>";
   };
   return this.festivalHTML;
 };
 
 FestivalList.prototype.images = function(){
-  for (var i = 0; i < 3; i++) {
+  var imageLimit
+  if(this.imagesJSON.length >= 9){
+    imageLimit = 9;
+  } else {
+    imageLimit = this.imagesJSON.length
+  }
+  for (var i = 0; i < imageLimit; i++) {
     this.imagesHTML +=
-    "<div class='image-box col-md-2'>" +
-    "<img class='image' id='image-" + i +
-    "' src='" + this.imagesJSON[i].url + "'/>" +
-    "<div class='image-name'>" + this.imagesJSON[i].name + "</div></div>"
+    "<div class='col-md-4'>" +
+      "<div class='thumbnail'>" +
+        "<a href='" + this.imagesJSON[i].url + "'>" +
+          "<img class='image-link' id='image-" + i + "' src='" + this.imagesJSON[i].url + "'/></a>" +
+      "<div class='image-name'>" + this.imagesJSON[i].name + "</div></div></div>";
   };
   return this.imagesHTML;
+};
+
+FestivalList.prototype.videos = function(){
+  var videoLimit
+  if(this.videosJSON.length >= 12){
+    videoLimit = 6;
+  } else {
+    videoLimit = this.videosJSON.length
+  }
+  for (var i = 0; i < videoLimit; i++) {
+    this.videosHTML +=
+    "<div class='col-md-6'>" +
+          "<iframe width='540' height='310' class='video' id='video-" + i + "' src='" + this.videosJSON[i].embed + "' frameborder='0' allowfullscreen></iframe>" +
+      "<div class='video-name'>" + this.videosJSON[i].name + "</div></div>";
+  };
+  return this.videosHTML;
 };
---
+++ src/index_interface.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $('#media-overlay').hide()
+  var displayFestivalList = new FestivalList();
+  var imagesHTML = displayFestivalList.images();
+  var videosHTML = displayFestivalList.videos();
+  document.getElementById("festival-list").innerHTML = displayFestivalList.list();
+  $("#img-btn").on('click', function() {
+    $("#media-overlay").toggle(500);
+    $("#main-content").toggle();
+    $("#media-title").text("IMAGES|2016");
+    document.getElementById("media-display").innerHTML = imagesHTML;
+  });
+  $("#vid-btn").on('click', function() {
+    $("#media-overlay").toggle(500);
+    $("#main-content").toggle();
+    $("#media-title").text("VIDEOS|2016");
+    document.getElementById("media-display").innerHTML = videosHTML;
+  });
+  $("#close").on('click', function() {
+    $("#media-overlay").toggle(500);
+    $("#main-content").toggle();
+  });
+});
