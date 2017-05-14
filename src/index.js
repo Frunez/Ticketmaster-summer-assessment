a69532534cc84f8c9fffb4c6d80d7e7503249fba
frunez <francesco.nunez@gmail.com>
2017-05-14 01:01:25 +0100

    styling content and adding image viewer

--- src/index.js
+++ src/index.js
@@ -2,17 +2,31 @@
 
 function FestivalList(){
   this.festivalJSON = festivals;
-  this.festivalHTML = ""
+  this.festivalHTML = "";
+  this.imagesJSON = images;
+  this.imagesHTML = "";
 }
 
 FestivalList.prototype.list = function(){
   for (var i = 0; i < this.festivalJSON.length; i++) {
     this.festivalHTML +=
-    "<li class='festival list-group-item col-md-8' id='festival-" + i + "'>" + "<a href=" + this.festivalJSON[i].url + ">" +
+    "<a href=" + this.festivalJSON[i].url + ">" +
+    "<li class='festival list-item' id='festival-" + i + "'>" +
     "<span class='festival-name'>" + this.festivalJSON[i].name + "</span>" +
-    "<span class='festival-location'>" + this.festivalJSON[i].location + "</span>" +
+    "<span class='festival-location'> &nbsp &nbsp" + this.festivalJSON[i].location + "</span>" +
     "<span class='festival-date'>" + this.festivalJSON[i].date + "</span>" +
-    "</a></li>";
+    "</li></a>";
   };
-  return this.festivalHTML
+  return this.festivalHTML;
+};
+
+FestivalList.prototype.images = function(){
+  for (var i = 0; i < 3; i++) {
+    this.imagesHTML +=
+    "<div class='image-box col-md-2'>" +
+    "<img class='image' id='image-" + i +
+    "' src='" + this.imagesJSON[i].url + "'/>" +
+    "<div class='image-name'>" + this.imagesJSON[i].name + "</div></div>"
+  };
+  return this.imagesHTML;
 };
